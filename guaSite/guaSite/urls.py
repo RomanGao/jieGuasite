f31d8d1e3585573ed69f32b88db01ca1a6507246
rowan <gaochl2014@126.com>
2024-07-08 19:49:30 +0500

    url add

--- guaSite/guaSite/urls.py
+++ guaSite/guaSite/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path("",include("jiegua.urls")),
     path("dinggua/",include("jiegua.urls")),
     path('admin/', admin.site.urls),
 ]
